7de8772183356ba1f74ce1b8e033ec8d75b6b040
Riko Kudo <rurikudo@ibm.com>
2023-10-17 17:59:10 +0900

    update variable container (#14)
    * fixed logic to detect undefined vars and support failed_when option
    
    Signed-off-by: Ruriko Kudo <rurikudo@ibm.com>
    
    * support vars in failed_when option
    
    Signed-off-by: Ruriko Kudo <rurikudo@ibm.com>
    
    * fixed not to detect the vars like x = x + y as undefined vars
    
    Signed-off-by: Ruriko Kudo <rurikudo@ibm.com>
    
    * removed unnecessary import
    
    Signed-off-by: Ruriko Kudo <rurikudo@ibm.com>
    
    ---------
    
    Signed-off-by: Ruriko Kudo <rurikudo@ibm.com>

--- sage_scan/process/variable_container.py
+++ sage_scan/process/variable_container.py
@@ -7,6 +7,7 @@ from sage_scan.process.variable_resolver import extract_variable_names
 import json
 import os
 import copy
+import re
 
 magic_vars = []
 vars_file = os.getenv("VARS_FILE", os.path.join(os.path.dirname(__file__),"ansible_variables.txt"))
@@ -17,6 +18,7 @@ with open(vars_file, "r") as f:
 @dataclass
 class VarCont:
     obj_key: str = ""
+    filepath: str = ""
     used_vars: {} = field(default_factory=dict)
     # set_vars: field(default_factory=dict)
     set_scoped_vars: {} = field(default_factory=dict) # available in children
@@ -33,6 +35,7 @@ class VarCont:
         return self.used_vars
 
 
+# return VarCont
 def to_vc(obj):
     vc = None
     if isinstance(obj, Playbook):
@@ -48,6 +51,7 @@ def to_vc(obj):
     return vc
 
 
+# return VarCont list
 def make_vc_arr(call_seq):
     vc_arr = {}
     for obj in call_seq:
@@ -81,12 +85,14 @@ def check_if_magic_vars(used_var):
         return True
     return False
 
+
 # return all vars in vc.used_vars but not in accum_vc.set_vars
 def find_undefined_vars(vc: VarCont, accum_vc: VarCont):
     undefined_vars = {}
     for v1_name, v1_val in vc.used_vars.items():
         in_scoped_vars = False
         in_local_vars = False
+        in_set_vars = False  # check with set vars in the same task when vars is used in 'failed_when'
         if check_if_magic_vars(v1_name):
             continue
         for v2 in accum_vc.set_scoped_vars:
@@ -107,17 +113,31 @@ def find_undefined_vars(vc: VarCont, accum_vc: VarCont):
                 break
         if in_local_vars:
             continue
+        if v1_name in vc.set_scoped_vars:
+            set_value = vc.set_scoped_vars[v1_name]
+            if v1_name in set_value:
+                # this supports the case like (x = x + y)
+                continue
+        if v1_val.get("in_failed_when", False):
+            for v2 in vc.set_scoped_vars:
+                if check_if_defined(v1_name, v2):
+                    in_set_vars = True
+                    break
+            if in_set_vars:
+                continue
         undefined_vars[v1_name] = v1_val
     return undefined_vars, vc.used_vars
 
 
-def compute_accum_vc(call_tree, vc_arr, obj_key):
+def compute_accum_vc(call_tree, vc_arr, obj_key, obj_filepath):
     parents=[]
     parents = traverse_and_get_parents(obj_key, call_tree, parents)
     parents.reverse()
     accum_vc = VarCont()
     for p in parents:
         pvc = vc_arr[p]
+        if pvc.filepath != obj_filepath:
+            continue
         accum_vc.accum(pvc)
     return accum_vc
 
@@ -159,7 +179,9 @@ def get_vc_from_playbook(playbook: Playbook):
 def get_vc_from_play(play: Play):
     vc = VarCont()
     vc.obj_key = play.key
+    vc.filepath = play.filepath
     vc.set_explicit_scoped_vars |= flatten_dict(play.variables)
+    # TODO: support vars_prompt
     return vc
 
 
@@ -167,6 +189,7 @@ def get_vc_from_play(play: Play):
 def get_vc_from_role(role: Role):
     vc = VarCont()
     vc.obj_key = role.key
+    vc.filepath = role.filepath
     vc.set_explicit_scoped_vars |= role.default_variables
     vc.set_explicit_scoped_vars |= role.variables
     return vc
@@ -176,6 +199,7 @@ def get_vc_from_role(role: Role):
 def get_vc_from_taskfile(taskfile: TaskFile):
     vc = VarCont()
     vc.obj_key = taskfile.key
+    vc.filepath = taskfile.filepath
     vc.set_explicit_scoped_vars |= taskfile.variables
     return vc
 
@@ -184,6 +208,7 @@ def get_vc_from_taskfile(taskfile: TaskFile):
 def get_vc_from_task(task: Task):
     vc = VarCont()
     vc.obj_key = task.key
+    vc.filepath = task.filepath
     vc.set_scoped_vars |= task.set_facts
     vc.set_scoped_vars |= task.registered_variables
     vc.set_local_vars |= task.variables
@@ -192,6 +217,7 @@ def get_vc_from_task(task: Task):
     return vc
 
 
+# return used vars in a task
 def used_vars_in_task(task: Task):
     options = task.options
     module_options = task.module_options
@@ -223,25 +249,47 @@ def extract_var_parts(options: dict):
     return vars_in_option
 
 
+# return used vars in when option
 def check_when_option(options):
     used_vars = {}
-    if "when" not in options:
+    if "when" not in options and "failed_when" not in options:
         return used_vars
-    when_value = options["when"]
+    when_value = options.get("when", "")
+    failed_when_value = options.get("failed_when", "")
     all_parts = []
     if type(when_value) == list:
         for v in when_value:
-            all_parts.extend(v.split())
+            all_parts.extend(re.split('[ |]', v))
     elif type(when_value) == dict:
         for v in when_value.values():
-            all_parts.extend(v.split())
+            all_parts.extend(re.split('[ |]', v))
     else:
-        all_parts = when_value.split()
+        all_parts = re.split('[ |]', when_value)
+
+    _used_vars = extract_when_option_var_name(all_parts)
+    used_vars |= _used_vars
+
+    all_parts = []
+    if type(failed_when_value) == list:
+        for v in failed_when_value:
+            all_parts.extend(re.split('[ |]', v))
+    elif type(failed_when_value) == dict:
+        for v in failed_when_value.values():
+            all_parts.extend(re.split('[ |]', v))
+    else:
+        all_parts = re.split('[ |]', failed_when_value)
+    _used_vars = extract_when_option_var_name(all_parts, is_failed_when=True)
+    used_vars |= _used_vars
+    return used_vars
+
+
+def extract_when_option_var_name(option_parts, is_failed_when=False):
+    used_vars = {}
     ignore_words = ["defined", "undefined", "is", "not", "and", "or", "|", "in", "none"]
     boolean_vars = ["True", "true", "t", "yes", 'y', 'on', "False", "false", 'f', 'no', 'n', 'off']
     data_type_words = ["bool", "float", "int", "length"]
-    for p in all_parts:
-        if "match(" in p:
+    for p in option_parts:
+        if "match(" in p or "default(" in p:
             continue
         p = p.replace(")","").replace("(","")
         if not p:
@@ -258,7 +306,10 @@ def check_when_option(options):
             continue
         if p.isdigit():
             continue
-        used_vars[p] = {"original": p, "name": p}
+        if is_failed_when:
+            used_vars[p] = {"original": p, "name": p, "in_failed_when": True}
+        else:
+            used_vars[p] = {"original": p, "name": p}
     return used_vars
 
 
@@ -311,13 +362,15 @@ def flatten_dict(d, parent_key='', sep='.'):
     return items
 
 
-def find_all_undefined_vars(call_tree, vc_arr):
+def find_all_undefined_vars(call_tree, vc_arr, target_filepath):
     undefined_vars = {}
     used_vars = {}
 
     accum_vc = VarCont()
     for vc in vc_arr.values():
-        accum_vc = compute_accum_vc(call_tree, vc_arr, vc.obj_key)
+        if vc.filepath != target_filepath:
+            continue
+        accum_vc = compute_accum_vc(call_tree, vc_arr, vc.obj_key, vc.filepath)
         und_vars, _used_vars = find_undefined_vars(vc, accum_vc)
         undefined_vars |= und_vars
         used_vars |= _used_vars
@@ -325,30 +378,15 @@ def find_all_undefined_vars(call_tree, vc_arr):
     return undefined_vars, used_vars
 
 
-def find_all_undefined_vars_in_pbdata(pb, vc_arr):
-    undefined_vars = []
-    used_vars = []
-
-    for vc in vc_arr.values():
-        accum_vc = compute_accum_vc(pb.call_tree, vc_arr, vc.obj_key)
-        und_vars = find_undefined_vars(vc, accum_vc)
-        undefined_vars.extend(und_vars)
-
-    used_vars = accum_vc.get_used_vars()
-    used_vars = [dict(t) for t in {tuple(sorted(d.items())): d for d in used_vars}.values()]
-    undefined_vars = [dict(t) for t in {tuple(sorted(d.items())): d for d in undefined_vars}.values()]
-    return undefined_vars, used_vars
-
-
-def find_all_set_vars(pd: PlaybookData|TaskFileData, call_tree, vc_arr, check_point=""):
+def find_all_set_vars(pd: PlaybookData|TaskFileData, call_tree, vc_arr, check_point=None):
     if not call_tree:
         return {}, {}
 
     all_set_vars = {}
     role_vars = {}
     if not check_point:
-        check_point = call_tree[-1][1].key
-    accum_vc = compute_accum_vc(call_tree, vc_arr, check_point)
+        check_point = call_tree[-1][1]
+    accum_vc = compute_accum_vc(call_tree, vc_arr, check_point.key, pd.object.filepath)
     if isinstance(pd, TaskFileData):
         if pd.role:
             parent_role = pd.role
@@ -383,7 +421,7 @@ def get_used_vars_from_data(pd: PlaybookData|TaskFileData):
     call_tree = pd.call_tree
     call_seq = pd.call_seq
     vc_arr = make_vc_arr(call_seq)
-    _, used_vars = find_all_undefined_vars(call_tree, vc_arr)
+    _, used_vars = find_all_undefined_vars(call_tree, vc_arr, pd.object.filepath)
     return used_vars
 
 
@@ -391,7 +429,7 @@ def get_undefined_vars_in_obj_from_data(pd: PlaybookData|TaskFileData):
     call_tree = pd.call_tree
     call_seq = pd.call_seq
     vc_arr = make_vc_arr(call_seq)
-    undefined_vars_in_obj, _ = find_all_undefined_vars(call_tree, vc_arr)
+    undefined_vars_in_obj, _ = find_all_undefined_vars(call_tree, vc_arr, pd.object.filepath)
     return undefined_vars_in_obj
 
 
@@ -400,7 +438,7 @@ def get_undefined_vars_value_from_data(pd: PlaybookData|TaskFileData):
     call_seq = pd.call_seq
     vc_arr = make_vc_arr(call_seq)
     set_vars = find_all_set_vars(pd, call_tree, vc_arr)
-    undefined_vars_in_obj, _ = find_all_undefined_vars(call_tree, vc_arr)
+    undefined_vars_in_obj, _ = find_all_undefined_vars(call_tree, vc_arr, pd.object.filepath)
     undefined_vars_value = get_undefined_vars_value(set_vars, undefined_vars_in_obj)
     return undefined_vars_value
 
@@ -410,7 +448,7 @@ def resolve_variables(pd: PlaybookData|TaskFileData):
     call_seq = pd.call_seq
     vc_arr = make_vc_arr(call_seq)
     set_vars, role_vars = find_all_set_vars(pd, call_tree, vc_arr)
-    undefined_vars_in_obj, used_vars = find_all_undefined_vars(call_tree, vc_arr)
+    undefined_vars_in_obj, used_vars = find_all_undefined_vars(call_tree, vc_arr, pd.object.filepath)
     undefined_vars_value = get_undefined_vars_value(set_vars, undefined_vars_in_obj)
     return set_vars, role_vars, used_vars, undefined_vars_in_obj, undefined_vars_value
 

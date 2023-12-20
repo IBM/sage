4fbe15497f4b803e3bc96003c15de4bfe981cad7
hirokuni-kitahara <hirokuni.kitahara1@ibm.com>
2023-12-20 15:24:58 +0900

    add a feature flag to determine if using variable container
    Signed-off-by: hirokuni-kitahara <hirokuni.kitahara1@ibm.com>

--- sage_scan/utils.py
+++ sage_scan/utils.py
@@ -254,7 +254,6 @@ def extract_variable_names(txt):
         if default_var_name != "":
             tmp_b["default"] = default_var_name
         blocks.append(tmp_b)
-<<<<<<< HEAD
     return blocks
 
 
@@ -272,6 +271,3 @@ def strtobool(val):
         return 0
     else:
         raise ValueError("invalid truth value %r" % (val,))
-=======
-    return blocks
->>>>>>> 44284be (move variable_container from process to sage_scan)
